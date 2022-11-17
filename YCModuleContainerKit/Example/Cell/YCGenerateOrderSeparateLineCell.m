89af44b24ae963a12c5bd31475238c84a8034b1d
Yancy <727732656@qq.com>
2022-11-17 18:04:52 +0800

    移除Masonry依赖

--- YCModuleContainerKit/Example/Cell/YCGenerateOrderSeparateLineCell.m
+++ YCModuleContainerKit/Example/Cell/YCGenerateOrderSeparateLineCell.m
@@ -7,7 +7,6 @@
 //
 
 #import "YCGenerateOrderSeparateLineCell.h"
-#import <Masonry.h>
 
 @interface YCGenerateOrderSeparateLineCell ()
 
@@ -27,10 +26,7 @@
 
 - (void)setupContent {
     [self.paddingContentView addSubview:self.separateLine];
-    [self.separateLine mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.left.right.bottom.equalTo(self.paddingContentView);
-        make.height.mas_equalTo(16);
-    }];
+    self.separateLine.frame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, 16);
 }
 
 - (UIView *)separateLine {
@@ -43,9 +39,8 @@
 
 - (void)setContainerCellModel:(id)containerCellModel {
     if ([containerCellModel isKindOfClass:[NSNumber class]]) {
-        [self.separateLine mas_updateConstraints:^(MASConstraintMaker *make) {
-            make.height.equalTo(containerCellModel);
-        }];
+        NSNumber *num = containerCellModel;
+        self.separateLine.frame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, num.floatValue);
     }
 }
 
